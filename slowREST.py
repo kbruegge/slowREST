438fa0667f2f4bd23b23a264be3faa07b73dbe28
Kai Brügge <kai.bruegge@udo.edu>
2015-03-02 13:39:28 +0100

    More text. And throw 404 for wrong resource

--- slowREST.py
+++ slowREST.py
@@ -4,6 +4,7 @@ from pymongo.errors import ConnectionFailure
 from bson import json_util
 from flask.ext.restful import reqparse
 from flask import render_template
+from flask import abort
 
 
 app = Flask(__name__)
@@ -16,6 +17,7 @@ app.config.update(dict(
     ))
 
 db = None
+names=[]
 
 
 def connect_to_db():
@@ -25,21 +27,19 @@ def connect_to_db():
 
 @app.route('/')
 def main_page():
-    names = list(db.collection_names())
-    names.remove('system.indexes')
     return render_template('index.html', names=names)
 
 
 @app.route('/services')
 def service_page():
-    names = list(db.collection_names())
-    names.remove('system.indexes')
     return json_util.dumps(names)
 
 
 
 @app.route('/aux/<attribute_name>', methods=['GET'])
 def aux_data(attribute_name):
+    if attribute_name not in names:
+        abort(404)
     parser = reqparse.RequestParser()
     parser.add_argument('from', type=float, help='start query at this time')
     parser.add_argument('to', type=float, help='latest time to query')
@@ -63,6 +63,8 @@ def aux_data(attribute_name):
 if __name__ == '__main__':
     try:
         db = connect_to_db()
+        names = list(db.collection_names())
+        names.remove('system.indexes')
         app.run()
     except ConnectionFailure:
         print("Could not connect to database. Tunnel configured?")
