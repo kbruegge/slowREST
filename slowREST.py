4436ca7eb15a40a24ef9f38be78cdaa9655189d9
Kai <kai.bruegge@gmail.com>
2015-03-02 09:33:49 +0100

    A simple flask server to serve website and json

---
+++ slowREST.py
@@ -0,0 +1,71 @@
+from flask import Flask
+from pymongo import MongoClient
+from pymongo.errors import ConnectionFailure
+from bson import json_util
+from flask.ext.restful import reqparse
+from flask import render_template
+
+
+app = Flask(__name__)
+app.debug = True
+
+app.config.update(dict(
+    DATABASE='aux',
+    HOST='localhost',
+    PORT=37017
+    ))
+
+db = None
+
+
+def connect_to_db():
+    client = MongoClient("mongodb://" + app.config["HOST"] + ":" + str(app.config["PORT"]) + "/" + app.config["DATABASE"])
+    return client.get_default_database()
+
+
+@app.route('/')
+def main_page():
+    names = list(db.collection_names())
+    names.remove('system.indexes')
+    return render_template('index.html', names=names)
+
+
+@app.route('/services')
+def service_page():
+    names = list(db.collection_names())
+    names.remove('system.indexes')
+    return json_util.dumps(names)
+
+
+
+@app.route('/aux/<attribute_name>', methods=['GET'])
+def aux_data(attribute_name):
+    parser = reqparse.RequestParser()
+    parser.add_argument('from', type=float, help='start query at this time')
+    parser.add_argument('to', type=float, help='latest time to query')
+    args = parser.parse_args(strict=True)
+
+    start = args.get('from')
+    end = args.get('to')
+
+    a = None
+    if start is None or end is None:
+        # print("No parameters")
+        a = []
+    else:
+        # print(start, end)
+        a = db[attribute_name].find({"Time": {"$gte": start, "$lt": end}})
+        # print("entries: " + str(a.count()))
+
+    return json_util.dumps(a)
+
+
+if __name__ == '__main__':
+    try:
+        db = connect_to_db()
+        app.run()
+    except ConnectionFailure:
+        print("Could not connect to database. Tunnel configured?")
+
+
+
